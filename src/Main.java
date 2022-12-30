c6a2508f89cc6b9507954ec310b2be2f7d66da15
PabasaraPalihena <pabapalihena@gmail.com>
2022-12-30 11:21:46 +0530

    find the summation of the array

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static Integer findsum(){
+
+        int arr[] = {1,2,3,4,5};
+        Scanner sc = new Scanner(System.in);
+//        int arr[] = new int [10];
+        int sum = 0;
+
+        System.out.println("Enter the elements of the array one by one ");
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+        return sum;
+    }
+    public static void main(String[] args) {
+        System.out.println("Output: "
+                + findsum());
+
+    }
+}
